d91bfce84a8436154ef6fbe2e575386a87c9ed2b
me <fv84697a@duck.com>
2025-08-08 12:39:16 +0200

    test

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 4000;
 
-app.get("/", (req, res) => {
+/*app.get("/", (req, res) => {
   res.send("Hello World!");
-});
+});*/
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
