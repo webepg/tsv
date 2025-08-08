45142a31d9e9f7b3302406b3995ba335376626bf
me <fv84697a@duck.com>
2025-08-08 13:29:40 +0200

    Daten vom Server holen

--- index.js
+++ index.js
@@ -10,6 +10,18 @@ const port = process.env.PORT || 4000;
 
 app.use(express.static(path.join(__dirname, "public")));
 
+// API-Route für Daten
+app.get("/api/data", (req, res) => {
+  // hier die Infos zusammenbauen
+
+  const data = {
+    title: "Meine Webseite",
+    heading: "Hallo Welt!",
+    message: "Willkommen auf meiner Webseite.",
+  };
+  res.json(data);
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
