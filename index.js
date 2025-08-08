86984631572df74cb296da2a7705e9618c30fd8f
me <fv84697a@duck.com>
2025-08-08 16:01:53 +0200

    test

--- index.js
+++ index.js
@@ -25,9 +25,10 @@ app.post("/api/match", (req, res) => {
   // Daten aus dem JSON-Body auswerten
   const matchData = req.body;
 
-  res
-    .status(200)
-    .json({ message: "Daten erfolgreich empfangen", data: matchData });
+  res.status(200).json({
+    message: "Daten erfolgreich empfangen",
+    data: JSON.stringify(matchData),
+  });
 });
 
 app.listen(port, () => {
