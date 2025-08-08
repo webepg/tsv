f5bac4f2a181a5fb1bf2dba609da68b6de6a5609
me <fv84697a@duck.com>
2025-08-08 12:10:54 +0200

    test

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 // Basic Node.js server example
-const http = require("http");
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 4000;
 
-const hostname = "127.0.0.1";
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello, World!\n");
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
 });
