79bbad9256ac70182e24651c973036ab3a015214
me <fv84697a@duck.com>
2025-08-18 20:00:23 +0200

    Rückbau Screenshot

--- index.js
+++ index.js
@@ -32,42 +32,6 @@ app.post("/api/match", (req, res) => {
   res.status(200).json(match);
 });
 
-app.post("/api/scr", (req, res) => {
-  // Daten aus dem JSON-Body auswerten
-
-  const puppeteer = require("puppeteer");
-
-  (async () => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto(
-      "https://www.fupa.net/match/sv-haarbach-m1-tsv-bad-griesbach-m1-250629/info"
-    );
-    await page.waitForNavigation({ waitUntil: "networkidle2" });
-    await page.screenshot({ path: "public/screenshot.png" });
-    await browser.close();
-  })();
-  res.status(200).json({});
-});
-
-const playwright = require("playwright");
-
-app.get("/api/screenshot", async (req, res) => {
-  (async () => {
-    const browser = await playwright.chromium.launch();
-    const page = await browser.newPage();
-    await page.goto(
-      "https://www.bfv.de/ergebnisse/spiel/-/02TJQLTF7O000000VS5489BTVV9SFN07"
-    );
-    await page.waitForNavigation({ waitUntil: "networkidle2" });
-    const screenshot = await page.screenshot({ path: "screenshot.png" });
-    await browser.close();
-    res.set("Content-Disposition", `attachment; filename="screenshot.png"`);
-    res.set("Content-Type", "image/png");
-    res.send(screenshot);
-  })();
-});
-
 // Alle Torschützen https://www.fupa.net/league/a-klasse-pocking/scorers
 
 // Beste TSV Torschützen https://www.fupa.net/team/tsv-bad-griesbach-m1-2025-26/playerstats
