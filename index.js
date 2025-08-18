60621c754875eee05a5f32a4519ea0c2173a0a73
me <fv84697a@duck.com>
2025-08-18 23:22:08 +0200

    Foto mit Puppeteer

--- index.js
+++ index.js
@@ -8,61 +8,10 @@ const puppeteer = require("puppeteer");
 
 app.use(express.static(path.join(__dirname, "public")));
 
-// API-Route für Daten
-app.get("/api/data", (req, res) => {
-  // hier die Infos zusammenbauen
-  const data = {
-    title: "Meine Webseite",
-    heading: "Hallo Welt!",
-    message: "Willkommen auf meiner Webseite.",
-  };
-  res.json(data);
-});
-
-app.post("/api/match", (req, res) => {
-  // Daten aus dem JSON-Body auswerten
-  const matchData = req.body;
-
-  let match = {
-    formattedDate: "250808",
-    homeTeam: "Heimteam",
-    awayTeam: "Auswärtsteam",
-    homeGoals: [{ minute: 90, additional: 2, goalscorer: "Heimtor" }],
-    awayGoals: [{ minute: 105, additional: 0, goalscorer: "Auswärtstor" }],
-  };
-
-  res.status(200).json(match);
-});
-
-/*
-app.post("/api", (req, res) => {
+app.get("/api/matches", async (req, res) => {
   console.log("Request:", req.body);
-  (async () => {
-    const browser = await playwright.chromium.launch();
-    const page = await browser.newPage();
-    await page.goto(
-      "https://www.fupa.net/match/sv-haarbach-m1-tsv-bad-griesbach-m1-250629/info"
-    );
-    await page.waitForLoadState("networkidle");
 
-    // Extrahieren von Daten direkt aus dem DOM
-    const title = await page.title();
-    const links = await page.$$eval("a", (links) =>
-      links.map((link) => link.href)
-    );
-
-    console.log(title);
-    console.log(links);
-    result = links;
-    await browser.close();
-  })();
-  res.status(200).json({});
-});
-*/
-
-app.post("/api", (req, res) => {
-  console.log("Request:", req.body);
-  (async () => {
+  async function getMatchDetails() {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto(
@@ -78,15 +27,101 @@ app.post("/api", (req, res) => {
 
     console.log(title);
     console.log(links);
-    result = links;
     await browser.close();
-  })();
-  res.status(200).json({});
+    return links;
+  }
+
+  let result = await getMatchDetails();
+
+  res.status(200).json(result);
+});
+
+app.post("/api/match", async (req, res) => {
+  console.log("Request:", req.body);
+
+  let match = {
+    formattedDate: "250808",
+    homeTeam: "Heimteam",
+    awayTeam: "Auswärtsteam",
+    homeGoals: [{ minute: 90, additional: 2, goalscorer: "Heimtor" }],
+    awayGoals: [{ minute: 105, additional: 0, goalscorer: "Auswärtstor" }],
+  };
+
+  async function getMatchDetails() {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(
+      "https://www.fupa.net/match/sv-haarbach-m1-tsv-bad-griesbach-m1-250629/info"
+    );
+    await page.waitForNetworkIdle();
+    const screenshot = await page.screenshot({ encoding: "binary" });
+    // Extrahieren von Daten direkt aus dem DOM
+    const title = await page.title();
+    const links = await page.$$eval("a", (links) =>
+      links.map((link) => link.href)
+    );
+
+    console.log(title);
+    console.log(links);
+    await browser.close();
+    return screenshot;
+  }
+
+  let result = await getMatchDetails();
+  res.set("Content-Type", "image/png");
+  res.send(result);
+});
+
+// Beste TSV Torschützen https://www.fupa.net/team/tsv-bad-griesbach-m1-2025-26/playerstats
+
+app.get("/api/scorers/tsv", async (req, res) => {
+  async function getTsvScorers() {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(
+      "https://www.fupa.net/team/tsv-bad-griesbach-m1-2025-26/playerstats"
+    );
+    await page.waitForNetworkIdle();
+
+    // Extrahieren von Daten direkt aus dem DOM
+    const title = await page.title();
+    const links = await page.$$eval("a", (links) =>
+      links.map((link) => link.href)
+    );
+
+    console.log(title);
+    console.log(links);
+    await browser.close();
+    return links;
+  }
+
+  let result = await getTsvScorers();
+  res.status(200).json(result);
 });
 
 // Alle Torschützen https://www.fupa.net/league/a-klasse-pocking/scorers
+app.get("/api/scorers", async (req, res) => {
+  async function getScorers() {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto("https://www.fupa.net/league/a-klasse-pocking/scorers");
+    await page.waitForNetworkIdle();
 
-// Beste TSV Torschützen https://www.fupa.net/team/tsv-bad-griesbach-m1-2025-26/playerstats
+    // Extrahieren von Daten direkt aus dem DOM
+    const title = await page.title();
+    const links = await page.$$eval("a", (links) =>
+      links.map((link) => link.href)
+    );
+
+    console.log(title);
+    console.log(links);
+    await browser.close();
+    return links;
+  }
+
+  let result = await getScorers();
+  res.status(200).json(result);
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
